6c06420fb30854404aea75db17be8258c9891d8b
Glory David <glodave99@gmail.com>
2020-05-18 14:46:18 +0100

    fixed stickler errors

--- enumerable.rb
+++ enumerable.rb
@@ -11,7 +11,8 @@ module Enumerable
   end
 
   def my_each_with_index
-    return to_enum unless block_given?  
+    return to_enum unless block_given?
+    
     n = 0
     while n <= size - 1
       yield(to_a[n], n)
@@ -21,6 +22,7 @@ module Enumerable
 
   def my_select
     return to_enum unless block_given? 
+
     result_arr = []
     my_each do |item|
       result_arr.push(item) if yield(item)
