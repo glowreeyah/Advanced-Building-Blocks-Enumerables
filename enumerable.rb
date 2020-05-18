3b1bdc8777218e6c19f44f2612793e7764cf342d
Glory David <glodave99@gmail.com>
2020-05-18 07:18:46 +0100

    Added tested my_each_with_index method

--- enumerable.rb
+++ enumerable.rb
@@ -1,3 +1,5 @@
+# rubocop: disable Metrics/ModuleLength, Metrics/MethodLength, Style/CaseEquality, Style/IfInsideElse
+
 module Enumerable
   def my_each
     return to_enum unless block_given?
@@ -8,4 +10,16 @@ module Enumerable
       n += 1
     end
   end
+
+  def my_each_with_index
+    return to_enum unless block_given?
+      
+    n = 0
+    while n <= size - 1
+      yield(to_a[n], n)
+      i += 1
+    end
+  end
+
+  
 end
